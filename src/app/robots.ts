cc98a05de7283b93f90673490e60b2e76c4d2e5e
“[firstname <bintesharif16@gmail.com>
2026-04-11 21:38:01 +0500

    robot file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,26 +3,32 @@ import { MetadataRoute } from 'next';
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
+      // Default rule: allow all crawlers to access everything except CRM
       {
         userAgent: '*',
         allow: '/',
         disallow: [
           '/crm/',
           '/crm/login/',
-          '/api/',
-          '/_next/',
         ],
       },
-      // Allow Googlebot and Bingbot full access (including above-the-fold content)
+      // Googlebot: explicitly allow all public content including Next.js assets
       {
         userAgent: 'Googlebot',
         allow: '/',
-        disallow: ['/crm/', '/api/'],
+        disallow: [
+          '/crm/',
+          '/crm/login/',
+        ],
       },
+      // Bingbot: same policy
       {
         userAgent: 'Bingbot',
         allow: '/',
-        disallow: ['/crm/', '/api/'],
+        disallow: [
+          '/crm/',
+          '/crm/login/',
+        ],
       },
     ],
     sitemap: 'https://www.italytaxiservice.com/sitemap.xml',
